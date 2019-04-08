f021e6e89726aa23d324aebb0cd2a275b63ab55b
tsejx <tsejx@foxmail.com>
2019-04-08 17:16:10 +0800

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+yarn run docs:build
+cd .vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:tsejx/javascript-guidebook.git master:gh-pages
+
+cd -
