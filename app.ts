4b9397e81f1b6ee6bbaae52f82b3964dd4226ced
mrsingsing <tsejx@foxmail.com>
2020-08-22 23:44:03 +0800

    :wrench: config: add nginx and http markdown language highlight

---
+++ app.ts
@@ -0,0 +1,4 @@
+import Prism from "prism-react-renderer/prism";
+(typeof global !== "undefined" ? global : window).Prism = Prism
+require("prismjs/components/prism-nginx")
+require("prismjs/components/prism-http")
